46a5cfbad82f342a2188af915b40ad0e231b690b
PedroMartinsFalleiros <pmfrp@hotmail.com>
2020-07-07 21:22:15 -0300

    removendo node modules

--- Node/aula13/routes.js
+++ Node/aula13/routes.js
@@ -3,15 +3,8 @@ const route = express.Router();
 const homeController = require('./SRC/controllers/homeController');
 const contatoController = require('./SRC/controllers/contatoController');
 
-function meuMiddleware(req,res, next){
-    console.log();
-    console.log('Passei no seu middleware');
-    console.log();
-    next();
-}
-
 //rotas da home
-route.get('/', meuMiddleware, homeController.paginaInicial);
+route.get('/', homeController.paginaInicial);
 route.post('/',homeController.trataPost);
 
 //rotas de contato
--- Node/aula13/server.js
+++ Node/aula13/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const route = require("./routes");
 const path = require('path');
+const {middlewareGlobal} = require('./SRC/middlewares/middleware');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.resolve(__dirname,'public')));
 app.set('views',path.resolve(__dirname,'SRC','views'));
 app.set('view engine', 'ejs');
 
+app.use(middlewareGlobal);
 app.use(route);
 
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
     console.log('Servidor executando na porta 3000');
-});
+}); 
