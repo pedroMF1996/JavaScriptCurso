5bc7daf6a2e5fb451589dc255e2ebe17b0af36e3
Pedro Martins Falleiros <pmfrp@hotmail.com>
2020-07-21 14:25:21 -0300

    Migrate e primeiros testes com a tabela alunos

--- ApiREST/src/controllers/HomeController.js
+++ ApiREST/src/controllers/HomeController.js
@@ -1,7 +1,17 @@
+import Aluno from '../model/Aluno';
+
 class HomeController {
-  index(req, res) {
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Maria',
+      sobrenome: 'Miranda',
+      email: 'mariam@hotmail.com',
+      idade: 21,
+      peso: 57.00,
+      altura: 1.60,
+    });
     res.status(200).json(
-      { 'Deu tudo certo denovo': true },
+      novoAluno,
     );
   }
 }
---
+++ ApiREST/src/database/index.js
@@ -0,0 +1,7 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+import Aluno from '../model/Aluno';
+
+const models = [Aluno];
+const connection = new Sequelize(databaseConfig);
+models.forEach((model) => model.init(connection));
