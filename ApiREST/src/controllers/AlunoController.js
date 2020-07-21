6d95ed669ebafd43e457fd443779d309a8254f27
Pedro Martins Falleiros <pmfrp@hotmail.com>
2020-07-21 20:48:49 -0300

    CRUD aluno

--- ApiREST/src/controllers/AlunoController.js
+++ ApiREST/src/controllers/AlunoController.js
@@ -7,6 +7,76 @@ class AlunoController {
       alunos,
     );
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.status(200).json(aluno);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({ errors: ['Faltando Id'] });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({ errors: ['Aluno não existe'] });
+      }
+
+      return res.status(200).json(aluno);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({ errors: ['Faltando Id'] });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({ errors: ['Aluno não existe'] });
+      }
+      await aluno.destroy();
+
+      return res.status(200).json({ apagado: true });
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({ errors: ['Faltando Id'] });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({ errors: ['Aluno não existe'] });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      return res.status(200).json(alunoAtualizado);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new AlunoController();
